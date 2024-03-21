bf033e7a6fd8a8b7a22911e9d7d00464a64929f5
Immanual <immanualatrinity@gmail.com>
2024-03-21 23:22:51 +0530

    Adding Interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  template: `
+  <h2>
+    Welcome {{name}}
+  </h2>
+  <h2>{{"2+2"}}</h2>
+  <h2>{{"Welcome " + name}}</h2>
+  <h2>{{name.length}}</h2>
+  <h2>{{name.toUpperCase()}}</h2>
+  <h2>{{greetUser()}}</h2>
+  <h2>{{siteUrl}}</h2>
+  `,
+  styles: []
 })
 export class TestComponent {
+  public name = "Immanual"
+  public siteUrl = window.location.href;
 
+  greetUser(){
+    return "Hello " + this.name;
+  }
 }
