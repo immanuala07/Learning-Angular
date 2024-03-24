78f77cf85476237e7ba35c8908668b033daeaee8
Immanual <immanualatrinity@gmail.com>
2024-03-24 19:17:17 +0530

    Style binding added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,38 +7,23 @@ import { Component } from '@angular/core';
     Welcome {{name}}
   </h2>
 
-  <h2>Immanual</h2>
+  <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
 
-  <h2 [class]="successClass">Immanual</h2>
+  <h2 [style.color]="highlightColor">Style Binding 2</h2>
 
-  <h2 class="text-special" [class]="successClass">Immanual</h2>
-
-  <h2 [class.text-danger]="hasError">Immanual</h2>
-
-  <h2 [ngClass]="messageClasses">Immanual</h2>
+  <h2 [ngStyle]="titleStyles">Style Binding 3</h2>
   `,
-  styles: [`
-    .text-success {
-      color:green;
-    }
-    .text-danger {
-      color:red;
-    }
-    .text-special {
-      font-style:italic;
-    }
-  `]
+  styles: []
 })
 export class TestComponent {
 
   public name = "Immanual";
-  public successClass = "text-success";
   public hasError = true;
   public isSpecial = true;
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
+  public highlightColor = 'orange';
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
   }
 
   greetUser(){
