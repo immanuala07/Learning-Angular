3bc60f38c5a20ad6e02dc3dc020efb6aec0f2db4
Immanual <immanualatrinity@gmail.com>
2024-03-24 10:58:48 +0530

    Class binding added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,15 +6,40 @@ import { Component } from '@angular/core';
   <h2>
     Welcome {{name}}
   </h2>
-  <input [id]="myId" type="text" value="Vishwas">
-  <input id="{{myId}}" bind-disabled="isDisabled" type="text" value="Vishwas">
+
+  <h2>Immanual</h2>
+
+  <h2 [class]="successClass">Immanual</h2>
+
+  <h2 class="text-special" [class]="successClass">Immanual</h2>
+
+  <h2 [class.text-danger]="hasError">Immanual</h2>
+
+  <h2 [ngClass]="messageClasses">Immanual</h2>
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color:green;
+    }
+    .text-danger {
+      color:red;
+    }
+    .text-special {
+      font-style:italic;
+    }
+  `]
 })
 export class TestComponent {
+
   public name = "Immanual";
-  public myId = "testId";
-  public isDisabled = false;
+  public successClass = "text-success";
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
 
   greetUser(){
     return "Hello " + this.name;
